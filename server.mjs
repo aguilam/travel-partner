b798cba1ce9441b8780e3eb93b580a60e098da1e
aguilam <pexfex@mail.ru>
2023-09-27 15:14:24 +0400

    added forecast for city by airport name

--- server.mjs
+++ server.mjs
@@ -16,6 +16,15 @@ app.use('/airport', async (req, res) => {
     res.status(500).json({ error: 'Internal server error' });
   }
 });
+app.use('/city', async (req, res) => {
+  try {
+    const responses = await fetch('https://api.travelpayouts.com/data/en/cities.json?_gl=1*15htavc*_ga*OTg4MDYwMDM1LjE2OTM5MzIyODY.*_ga_1WLL0NEBEH*MTY5NTgxMTc1Ni4yNC4xLjE2OTU4MTIxODguNTIuMC4w');
+    const data = await responses.json();
+    res.json(data);
+  } catch (error) {
+    res.status(500).json({ error: 'Internal server error' });
+  }
+});
 app.use('/', async (req, res) => {
   try {
     const response = await fetch('https://api.travelpayouts.com/v1/prices/calendar?departure_date=2023-10&origin=MOW&destination=BCN&calendar_type=departure_date&token=b6504ed905463961a565613fecd058df');
