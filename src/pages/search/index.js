4d2c4d215d404666acc9fbb262f79cf8b1dc6796
aguilam <pexfex@mail.ru>
2023-09-29 21:38:08 +0400

    added a Modal with info about ticket

--- src/pages/search/index.js
+++ src/pages/search/index.js
@@ -1,4 +1,6 @@
+import { useState } from 'react';
 import '../../app/page.css'
+import { Button, Modal } from 'antd';
 export async function getServerSideProps() {
   const response = await fetch('http://localhost:3001');
   const dataset = await response.json();
@@ -11,12 +13,34 @@ export async function getServerSideProps() {
 }
 
 export default function Page({ tickets }) {
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  const [ModalInfo, setModalInfo] = useState([]);
+
+const showModal = (ticket) => {
+  setIsModalOpen(true);
+  setModalInfo(ticket)
+  console.log('nhhu',ModalInfo)
+};
+
+const handleOk = () => {
+  setIsModalOpen(false);
+};
+
+const handleCancel = () => {
+  setIsModalOpen(false);
+};
   console.log(tickets);
   return (
     <div className='Feature__containers'>
       {tickets.map((ticket, index) => {
         return (
-          <div key={index} className='Feature__info_containers'>
+          <>
+          <Button 
+            type="primary" 
+            onClick={() => showModal(ticket)}
+            key={index} 
+            className='Feature__info_containers'
+          >
             <p className='Feature__descriptions'>{ticket.origin}</p>
             <p className='Feature__descriptions'>{ticket.destination}</p>
             <p className='Feature__descriptions'>{ticket.airline}</p>
@@ -26,7 +50,24 @@ export default function Page({ tickets }) {
             <p className='Feature__descriptions'>{ticket.price}</p>
             <p className='Feature__descriptions'>{ticket.flight_number}</p>
             <p className='Feature__descriptions'>{ticket.transfers}</p>
-          </div>
+          </Button>
+          <Modal
+          title="Title"
+          open={isModalOpen}
+          onOk={handleOk}
+          onCancel={handleCancel}
+          >
+          <p>{ModalInfo.origin}</p>
+          <p>{ModalInfo.destination}</p>
+          <p>{ModalInfo.airline}</p>
+          <p>{ModalInfo.departure_at}</p>
+          <p>{ModalInfo.return_at}</p>
+          <p>{ModalInfo.expires_at}</p>
+          <p>{ModalInfo.price}</p>
+          <p>{ModalInfo.flight_number}</p>
+          <p>{ModalInfo.transfers}</p>
+          </Modal>
+        </>
         );
       })}
     </div>
